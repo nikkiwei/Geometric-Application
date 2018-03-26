3dbaebaf4bdf1f75156eb8cdb88a9b4c4226a418
oneal22i@mtholyoke.edu <Ivy O'Neal-Odom@138.110.225.177>
2018-03-26 18:46:53 -0400

    Running TestWrapper will run each test class

--- MainApp/src/SystemTest/TestWrapper.java
+++ MainApp/src/SystemTest/TestWrapper.java
@@ -4,7 +4,8 @@ import org.sikuli.script.FindFailed;
 import org.sikuli.script.Screen;
 
 /**
- * Proof of concept that the Sikuli code can be in the same project as the Geometry App
+ * Runs each test class
+ * The test classes must be added to the "test()" function
  * @author Ivy O'Neal-Odom
  *
  */
@@ -12,9 +13,32 @@ public class TestWrapper {
 	static Screen s = new Screen();
 	static String OS;
 	
+	/**
+	 * Calls the various tests
+	 * @param args N/A
+	 */
 	public static void main(String[] args) {
-		getOS();
-		selectApp();
+		getOS(); //First we need to get the OS
+		
+		//Test Start
+		DrawLineTest.main(args);
+		try {
+			s.click(OS + "close.png");
+		} catch (FindFailed e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//Test End
+		
+		//Test Start
+		DrawPointTest.main(args);
+		try {
+			s.click(OS + "close.png");
+		} catch (FindFailed e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//Test End
 	}
 	
 	/**
@@ -22,23 +46,11 @@ public class TestWrapper {
 	 * Useful for a shorthand when finding file structures
 	 * Also useful because things look different on macs than on pcs
 	 */
-	public static void getOS() {
+	private static void getOS() {
 		if(System.getProperty("os.name").contains("Windows")) {
 			OS = "SystemTestImages/windows/";
 		}else {
 			OS = "SystemTestImages/mac/";
-		}
-	}
-	
-	/**
-	 * Selects the icon of the Geometry App from the taskbar
-	 * Note: the Application should be running already
-	 */
-	public static void selectApp() {
-		try {
-			s.click(OS + "appToolbar.PNG");
-		}catch(FindFailed f) {
-			f.printStackTrace();
 		}
 	}
 }
