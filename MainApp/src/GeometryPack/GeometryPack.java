d121fcd03cc0a387e5433148a8392ee1ab2dd88e
nguye84t <31997423+nguye84t@users.noreply.github.com>
2018-03-28 17:16:21 -0400

    Update GeometryPack.java
    Added rotation about a point function with 2 supporting functions.

--- MainApp/src/GeometryPack/GeometryPack.java
+++ MainApp/src/GeometryPack/GeometryPack.java
@@ -202,6 +202,66 @@ public class GeometryPack {
 		return point_C;
 
 		}
+	/**
+	 * Calculate distance between two points
+	 * @param point_1
+	 * @param point_2
+	 * @return distance
+	 * @author Nguyen Nguyen
+	 */
+	private static double calculate_distance(Point2D point_1, Point2D point_2) {
+		return point_1.distance(point_2);
+	}
+	
+	/**
+	 * Return an arraylist of point2D of corresponding to the reflection about the 
+	 * rotation point of the list of Point2D inputs
+	 * @param list_vertices
+	 * @param rotate_point
+	 * @return
+	 * @author Nguyen Nguyen
+	 */
+	private static ArrayList<Point2D> rotatePoint( ArrayList<Point2D> list_vertices, Point2D rotate_point){
+		ArrayList<Point2D> new_list_vertices = new ArrayList(list_vertices.size());
+		for (int i = 0; i < list_vertices.size(); i++) {
+			new_list_vertices.add(i, angle_replication(rotate_point, Math.PI, list_vertices.get(i)));
+			System.out.println(new_list_vertices.get(i).getX() + "  " + new_list_vertices.get(i).getY());
+		}
+		
+		return new_list_vertices;
+	}
 
+	/**
+	 * Second version of angle_replication
+	 * Does the same thing, just take in different parameters
+	 * @param point_B
+	 * @param alpha
+	 * @param point_A
+	 * @return
+	 * @author Nguyen Nguyen
+	 */
+	private static Point2D angle_replication ( Point2D point_B, double alpha, Point2D point_A ){
 
+		// Calculate length of AB 
+		double dx = point_A.getX() - point_B.getX();
+		double dy = point_A.getY() - point_B.getY();
+		double length = calculate_distance(point_B, point_A);
+		System.out.println(dx + "  " + dy);
+		// Compute the angle AB makes with the horizontal axis
+		double angle_AB = Math.atan(dx/dy);
+		
+		// Compute the difference between alpha and the angle AB makes with the horizontal axis (or basically, the angle BC makes with the horizontal axis)
+		double angle_BC = alpha - angle_AB;
+		System.out.println(angle_BC);
+		// Calculate coordinates of point C
+		double xC = point_B.getX() + length*Math.sin(angle_BC);
+		double yC = point_B.getY() - length*Math.cos(angle_BC);
+
+		// Construct point C
+		Point2D point_C = new Point((int) xC,(int) yC);
+
+		// Return point C
+		return point_C;
+
+		}
 	}
